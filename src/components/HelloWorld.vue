18e1d5cdc258e6ea812316d51f8955a1ae5e887f
Vitor <Vitor>
2024-12-05 14:37:19 -0300

    Iniciando o projeto com modificações preliminares

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Componente HelloWorld</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
